98ed1e341638c764c55a5bbd17eeedf6d4736ffd
LogicalOgbonna <arinzeogbo@gmail.com>
2020-10-25 16:35:36 +0100

    chore(npm): added .npmignore

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,7 +4,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "build"),
         filename: "index.js",
-        publicPath: '/build/',
+        publicPath: './',
     },
     entry: "./src/index.js",
     module: {
